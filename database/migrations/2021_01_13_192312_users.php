eb8cfa4cb0f162963fcbbb134ce9c208d4e6427d
Farhan Agung Maulana <famagung@gmail.com>
2021-01-14 02:55:08 +0700

    + Make Migration & Seeder

--- database/migrations/2021_01_13_192312_users.php
+++ database/migrations/2021_01_13_192312_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Users extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('email', 191)->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('mobile_number');
+            $table->string('balance');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'email' => 'farhanagungmaulana@gmail.com',
+            'name' => 'farhan agung maulana',
+            'password' => Crypt::encrypt('12345678'),
+            'mobile_number' => '081336503277',
+            'balance' => 0
+        ]);
     }
 }
